afb2039c1e787e23792df842f8f2738a9a50ec2e
Damian <d.pagowski@gmail.com>
2019-10-06 11:09:47 +0700

    serving html, static files and json

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 const express = require('express')
-const app = express()
+const app = express() // create application object
+
+app.use(express.static(__dirname + '/views'))
+
+
+
+app.use(function middleware(req, res, next) {
+    var string = req.method + ' ' + req.path + ' - ' + req.ip;
+    console.log(string)
+    next();
+  });
+
 const port = 3000
+const indexAbsolutePath = __dirname + '/views/index.html'
+// added first route
+app.get(
+  '/',
+  (req, res) => res.sendFile(indexAbsolutePath)
+  // res.send('Response String')
+)
 
-app.get('/', (req, res) => res.send('Response String'))
+app.get('/error', function (req, res, next) {
+  setTimeout(function () {
+    try {
+      throw new Error('BROKEN')
+    } catch (err) {
+      next(err)
+    }
+  }, 100)
+})
 
+app.get('/json', function (req, res) {
+  res.json({ message: 'Hello World' })
+})
+
+// start server, listening on port 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
